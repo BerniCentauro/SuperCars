3d334c479bbe3be05138e91b2ebdd0bd4dbb108b
Berni Hidalgo <berni@centaurosolutions.com>
2016-11-29 16:44:43 -0600

    Lista de Vehiculos

---
+++ MvcInterface/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccess;
+using Context;
+
+namespace MvcInterface.Controllers
+{
+    public class HomeController : Controller
+    {
+        private VehiclesData _data = new VehiclesData();
+
+        // GET: Home
+        public ActionResult Index(int page = 1)
+        {
+            int totalRows = 0;
+
+            List<Vehicle> vehiclesList = _data.Page(page, 10, out totalRows);
+
+            ViewBag.Vehicles = vehiclesList;
+            return View();
+        }
+    }
+}
---
+++ MvcInterface/Controllers/VehiclesController.cs
@@ -0,0 +1,49 @@
+﻿using Context;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcInterface.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private VehiclesData _data = new VehiclesData();
+
+        // GET: Vehicles
+        public ActionResult Index(int page = 1)
+        {
+            int totalRows = 0;
+
+            List<Vehicle> vehiclesList = _data.Page(page, 10, out totalRows);
+
+            ViewBag.Vehicles = vehiclesList;
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Vehicle pVehicle)
+        {
+            try
+            {
+                _data.Insert(pVehicle);
+                Response.Redirect("~/Vehicles");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Ha ocurrido un error";
+                return View();
+            }
+
+        }
+    }
+}
