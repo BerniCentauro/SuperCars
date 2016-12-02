0fd737f6a802673d81034659ae822a80853872b6
Berni Hidalgo <berni@centaurosolutions.com>
2016-12-01 19:43:34 -0600

    Paginación completa

--- MvcInterface/Controllers/HomeController.cs
+++ MvcInterface/Controllers/HomeController.cs
@@ -3,15 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataAccess;
+using Context;
+using MvcInterface.Models;
 
 namespace MvcInterface.Controllers
 {
     public class HomeController : Controller
     {
+        private VehiclesData _data = new VehiclesData();
+        private const int ROWS_PER_PAGE = 1;
+
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            int totalRows = 0;
+
+            List<Vehicle> vehicleList = _data.Page(page, ROWS_PER_PAGE, out totalRows);
+
+            Pagination<Vehicle> objPagination = new Pagination<Vehicle>();
+            objPagination.Items = vehicleList;
+            objPagination.PageNumber = page;
+            objPagination.RowsPerPage = ROWS_PER_PAGE;
+            objPagination.TotalRows = totalRows;
+            objPagination.TotalPages = totalRows / ROWS_PER_PAGE;
+
+            List<int> pagesList = GetPages(objPagination.TotalPages);
+
+            ViewBag.Pages = pagesList;
+
+            return View(objPagination);
+        }
+        
+        private List<int> GetPages(int totalPages)
+        {
+            List<int> pages = new List<int>();
+
+            for (int i = 1; i <= totalPages; i++)
+            {
+                pages.Add(i);
+            }
+
+            return pages;
         }
     }
 }
---
+++ MvcInterface/Models/Pagination.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcInterface.Models
+{
+    public class Pagination<T>
+    {
+        public List<T> Items { get; set; }
+
+        public int RowsPerPage { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public int TotalRows { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
